ca7e280919b002e53840dd13480840b67cab437d
Andrei <example@gmail.com>
2022-06-25 14:04:19 +0300

    tiny slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,23 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-      speed: 1200,
-      adaptiveHeight: false,
-      prevArrow:'<button type="button" class="slick-prev"><img src="img/carousel/chevron-left.png" alt="chevron_left"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/chevron-right.png" alt="chevron_right"></button>',
-      responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                dots: true,
-                arrows: false
-            }
-        }
-      ]
-    });
-  });
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  controls: false,
+  nav: false,
+  navPosition: "bottom",
+  responsive: {
+    320: {
+      nav: true
+    },
+    992: {
+      nav: false
+    }
+  }
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
