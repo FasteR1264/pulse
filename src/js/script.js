da4cccb940f65e1fc28527d7a8a125a528d44489
Andrei <example@gmail.com>
2022-06-26 11:07:21 +0300

    owl carousel added

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,56 @@
 
-const slider = tns({
-  container: '.carousel__inner',
-  items: 1,
-  slideBy: 'page',
-  controls: false,
-  nav: false,
-  navPosition: "bottom",
-  responsive: {
-    320: {
-      nav: true
-    },
-    992: {
-      nav: false
+// const slider = tns({
+//   container: '.carousel__inner',
+//   items: 1,
+//   slideBy: 'page',
+//   controls: false,
+//   nav: false,
+//   navPosition: "bottom",
+//   responsive: {
+//     320: {
+//       nav: true
+//     },
+//     992: {
+//       nav: false
+//     }
+//   }
+// });
+// document.querySelector('.next').addEventListener('click', function () {
+//   slider.goTo('next');
+// });
+// document.querySelector('.prev').addEventListener('click', function () {
+//   slider.goTo('prev');
+// });
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    items: 1,
+    mouseDrag: false,
+    touchDrag: false,
+    nav: false,
+    center: true,
+    responsive:{
+        0:{
+          dots: true,
+          items: 1,
+          mouseDrag: true,
+          touchDrag: true,
+        },
+        992:{
+          mouseDrag: true,
+          dots: false,
+          items: 1
+        }
     }
-  }
+})
+const owl = $('.owl-carousel');
+owl.owlCarousel();
+$('.next').click(function() {
+  owl.trigger('next.owl.carousel');
+})
+$('.prev').click(function() {
+  owl.trigger('prev.owl.carousel');
+})
 });
-document.querySelector('.next').addEventListener('click', function () {
-  slider.goTo('next');
-});
-document.querySelector('.prev').addEventListener('click', function () {
-  slider.goTo('prev');
-});
+
